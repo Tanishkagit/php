84557c0c333ed328b18a89709f6fd7b3b9b18d38
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-04-07 21:57:41 +0530

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -9,3 +9,13 @@ echo "connection successful connected";
 else{
 echo "connection is not connected ";
 }
+$query="CREATE DATABASE tanishka";
+$check=mysqli_query($connection,$query);
+if($check){
+echo "database created ";
+}
+else{
+echo "error";
+}
+?>
+
