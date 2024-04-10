774d2e97ea99247e363f126b7ca0509e5c6716ca
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-04-10 21:23:21 +0530

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -17,5 +17,14 @@ echo "database created ";
 else{
 echo "error";
 }
+
+$query1="CREATE TABLE student";
+$check1=mysqli_query($connection,$query1);
+if($check1){
+echo "table is created ";
+}
+else{
+echo "error";
+}
 ?>
 
