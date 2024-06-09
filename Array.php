70eeed7631331cdb76b76cc81eab61332468f07d
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-06-09 21:18:37 +0530

    Array.php

--- Array.php
+++ Array.php
@@ -2,3 +2,16 @@
 $array=array("ram","shyam");
 echo $array;
 ?>
+
+
+<?php
+$array=array(1,2,3,4,5,6);
+echo $array[0];
+echo $array[1];
+echo $array[2];
+echo $array[3];
+echo $array[4];
+echo $array[5];
+?>
+
+
